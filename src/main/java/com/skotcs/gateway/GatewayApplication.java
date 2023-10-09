59f6c39b1b4413c9565bef15ee6149b2a04a64ab
stilisky <setonde.hounnouvi@epitech.eu>
2023-10-09 13:05:37 +0100

    Gateway configuration

--- src/main/java/com/skotcs/gateway/GatewayApplication.java
+++ src/main/java/com/skotcs/gateway/GatewayApplication.java
@@ -2,12 +2,31 @@ package com.skotcs.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/*import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;*/
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+	@Bean
+	RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r->r.path("/service1/**").uri("http://localhost:4040/"))
+				.build();
+	}
+
+	/*DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties prop) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, prop);
+	}*/
 
 }
